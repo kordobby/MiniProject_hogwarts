915210b7b4d97656c43097afe0f72d2077454ee4
kordobby <poordobby@naver.com>
2022-05-22 12:58:33 +0900

    Create Home(page)

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,11 +1,27 @@
 /* Home.jsx */
+import styled from "styled-components";
+
+// Import Header Menu Components
+import HeaderNavi from "../Components/HeaderNavi.jsx";
+import TestImg from '../img/background.png';
 
 const Home = () => {
     return (
         <>
-         This is Home
+            {/* Header Menu : fixed Component */}
+            <HeaderNavi></HeaderNavi>
+
+            {/* background img => 동영상으로 바꿀 예정 */}
+            <HomeMain>
+            <img src={TestImg} alt = "배경"></img>
+            </HomeMain>
         </>
     )
 }
 
 export default Home;
+
+const HomeMain = styled.div`
+  width : 100%;
+  height: 100vh;
+`;
