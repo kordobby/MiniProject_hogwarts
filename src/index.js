8dabede5d0e22c4b64a785edcb5e00a362b49ef9
kordobby <poordobby@naver.com>
2022-05-22 12:56:16 +0900

    // add BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
 // Global CSS
 import GlobalStyled from "./Components/GlobalStyled/GlobalStyled.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <>
+    <BrowserRouter>
         {/* Global CSS */}
         <GlobalStyled />
         <App />
-    </>
+    </BrowserRouter>
 );
