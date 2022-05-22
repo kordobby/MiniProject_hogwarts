09bbd014b4294a9f4f7f783c4f0b6abcaa62adee
kordobby <poordobby@naver.com>
2022-05-22 12:58:49 +0900

    Create Gallery(page)

---
+++ src/Routes/Gallery.jsx
@@ -0,0 +1,16 @@
+/* Home.jsx */
+
+// Import Header Menu Components
+import HeaderNavi from "../Components/HeaderNavi.jsx";
+
+const Gallery = () => {
+    return (
+        <>
+        {/* background img => 동영상으로 바꿀 예정 */}
+        <HeaderNavi> </HeaderNavi>
+         This is Gallery
+        </>
+    )
+}
+
+export default Gallery;
