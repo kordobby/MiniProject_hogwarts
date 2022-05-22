ce51f180af540a1f363c831b8decd6342b5a6759
kordobby <poordobby@naver.com>
2022-05-22 12:59:34 +0900

    add Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 /* App.jsx */
 
+// import Router
+import { Route, Routes } from 'react-router-dom';
+
 //import Home from './Routes/Home.jsx'
-import IntroMovie from './Routes/IntroMovie.jsx'
+import Home from './Routes/Home.jsx'
+import IntroMovie from './Routes/IntroMovie.jsx';
+import Gallery from './Routes/Gallery.jsx'
+import House from './Routes/House.jsx'
+
 
 
 const App = () => {
   return (
-    <>
-        <IntroMovie />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/house" element={<House />}></Route>
+      <Route path="/gallery" element={<Gallery />}></Route>
+      <Route path="/introMovie" element={<IntroMovie />}></Route>
+    </Routes>
   );
 }
 
